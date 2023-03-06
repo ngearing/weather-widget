31f98de78353b49d38f3aa8e7cd3b4edb3734229
Nathan Gearing <natgearing@gmail.com>
2023-03-07 10:55:13 +1100

    Update - widget respsonsiveness

--- weather-widget.php
+++ weather-widget.php
@@ -5,10 +5,12 @@
  * Plugin URI: https://bitbucket.org/ngearing/weather-widget/
  * Author: Nathan
  * Description: A weather widget.
- * Version: 0.1.1
+ * Version: 0.1.2
  *
  * Update URI: https://bitbucket.org/ngearing/weather-widget/
  * download_url: https://bitbucket.org/ngearing/weather-widget/
+ *
+ * @package weather-widget
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
